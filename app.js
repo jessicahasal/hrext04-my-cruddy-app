6bd01b174693f8e6953602a447bfeb29b52fb867
Jessica Hasal <jessicahasal96@gmail.com>
2018-09-13 18:09:15 -0700

    functioning buttons and local storage

--- app.js
+++ app.js
@@ -1,65 +1,98 @@
 $(document).ready(function() {
 
+  var displayEntries = function(){
+    var keys = Object.keys(localStorage)
+    var $entries = $('.entries');
+    $entries.html('')
+
+    for(var i = 0; i < keys.length; i++){
+    
+      var $box = $('<div class="box"></div>');
+      var $entry = $('<div class="entry"></div>');
+      var $journalMessage = $('<div class="journalMessage"></div>');
+    
+      var journalObj = JSON.parse(localStorage[keys[i]]) ;
+      var message = journalObj.message ;
+      $entry.text(keys[i])
+      $entry.prependTo($box);
+      $journalMessage.text(message);
+      $journalMessage.appendTo($box);
+      $box.appendTo($entries);  
+    }
+  }
+  displayEntries()  
+  //end of refresh page button
 
   $(".add-text-btn").on("click", function(){
+   
+    var journalObj = {}
+    
+    let title = $(".user-input-title").val();
+    let message = $(".user-input-message").val();
+    
 
-    // store values
-    let inputKey = $(".user-input-title").val();
-    let inputValue = $(".user-input-body").val();
+    if(title === ''){
+      alert("Cannot submit empty form")
+    } else {
+      $(".user-input-title").val("");
+      $(".user-input-message").val("");
 
-    // clear values
-    $(".user-input-title").val("");
-    $(".user-input-body").val("");
 
-    console.log(inputKey, inputValue);
+      journalObj['message'] = message;
 
-    localStorage.setItem(inputKey, inputValue);
-    // data-
-    let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
-    $(".display").html(itemHtml);
-    //console.log(localStorage);
-    // how can we delegate this event to the outer html node?
-    // https://learn.jquery.com/events/event-delegation/
 
-    $(".display-item").on("click", function(e){
-      // plop the key:value back into the input boxes
+      localStorage.setItem(title, JSON.stringify(journalObj));
+    
+      var localStorageKeys = Object.keys(localStorage)
+      var $entries = $('.entries');
+      $entries.html('')
 
-      // get the values from the the divs?
-      console.log("key=> ", e.target.dataset.storageKey); // user-input-title
-      localStorage.getItem(e.target.dataset.storageKey); // user-input-body
-
-      // set those values in the form fields
-      $(".user-input-title").val(e.target.dataset.storageKey);
-      $(".user-input-body").val(localStorage.getItem(e.target.dataset.storageKey));
-    });
+      displayEntries() 
+    }  
+  });
+  //end of click 'add text button'
+  
+  $(".entries").on("click", ".entry", function(e){
+    var journalObj = JSON.parse(localStorage.getItem(e.target.innerText))
+    
+    $(".user-input-title").val(e.target.innerText);
+    $(".user-input-message").val(journalObj.message);
 
   });
 
+  $(".del-text-btn").on("click", function() {
+    alert('ITEM DELETED');
+    localStorage.removeItem( $('.user-input-title').val() );
+    $(".user-input-title").val("");
+    $(".user-input-message").val("");
 
 
-   // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
-
-   $(".del-text-btn").on("click", function() {
-     alert('item deleted? check the console'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
-     $(".user-input-title").val("");
-     $(".user-input-body").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
-   });
-
+    displayEntries();
+  });
 
    // iterative approach to adding items
    // store data as stringified array of objects
    // store data with individual keys
   // how do we get keys? research Object.keys
 
+  //opponent, date, points, assists, steals, rebounds, blocks
+  $("#happy-button").on("click", function() {
+     $("body").first().css("background-color", "#ffff66");
+  });
+  $("#sad-button").on("click", function() {
+     $("body").first().css("background-color", "#0099cc");
+  });
+  $("#mad-button").on("click", function() {
+     $("body").first().css("background-color", "#ff3300");
+
+  });
 
 
 });
+
+
+
+
+
+
+  //opponent, date, points, assists, steals, rebounds, blocks
